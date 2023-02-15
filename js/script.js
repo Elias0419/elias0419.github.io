3b0a7b31b24131aefafb61bbb1f90a5c7b67e95b
Elias0419 <108772953+Elias0419@users.noreply.github.com>
2023-02-14 20:09:53 -0500

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+var myTextArea = document.getElementById("my-textarea");
+var myCodeMirror = CodeMirror.fromTextArea(myTextArea, {
+    lineNumbers: true,
+    mode: "javascript",
+    theme: "dracula"
+});
