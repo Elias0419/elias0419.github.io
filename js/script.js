133de61a12d6b0c12fd31076900a7ff6b96382c7
Elias0419 <108772953+Elias0419@users.noreply.github.com>
2023-02-14 20:23:23 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,5 +3,5 @@ var myTextArea = document.getElementById("my-textarea");
 var myCodeMirror = CodeMirror.fromTextArea(myTextArea, {
     lineNumbers: true,
     mode: "javascript",
-    theme: "dracula"
+   // theme: "dracula"
 });
