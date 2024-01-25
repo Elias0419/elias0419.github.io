b63c6e60afd5aa2b23e72f16da904e683f4aba94
Elias0419 <108772953+Elias0419@users.noreply.github.com>
2024-01-25 12:18:13 -0500

    Delete js/script.js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-
-window.onload = function() {
-  var myTextArea = document.getElementById("my-textarea");
-  var myCodeMirror = CodeMirror.fromTextArea(myTextArea, {
-    lineNumbers: true,
-    mode: "javascript",
-    theme: "dracula"
-  });
-};
